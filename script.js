7fd89f34269e7a8e82562faa58944d5d10992e93
Dan Lo <nflwwe71@gmail.com>
2020-03-28 09:36:54 -0400

    draft uno

---
+++ script.js
@@ -0,0 +1,12 @@
+function sum(num) {
+    total = num + num + num;
+    return total;
+}
+sum()
+
+function average(num) {
+    total = num + num + num;
+    avg = total / 3;
+    return avg;
+}
+average()
